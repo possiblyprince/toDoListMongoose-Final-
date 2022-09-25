53f57db45d3d8fc7e514ea35f63906c2356be137
Prince <princedhakal@outlook.com>
2022-09-25 14:35:21 +0545

    Implemented mongoose

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
-
+ // Code from old modules are commented out 
+ //So that you can compare
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,14 +12,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+  if (err){
+    console.log(err);
+  }
+  else{
+    console.log("Successfully saved default items to DB");
+  }
+});
+
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
